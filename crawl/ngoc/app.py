32cabc7783577adba8a3babd88d8235bf2a2cec6
quangngoc430 <ngoc.bui150019@vnuk.edu.vn>
2019-07-03 16:29:31 +0700

    Add 2 files csv

--- crawl/ngoc/app.py
+++ crawl/ngoc/app.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import urllib.request
 import os
 import csv
+import pandas
 import json
 import philong
 
@@ -29,19 +30,33 @@ def get_resize_image_url(url_image):
     index = url_image.rfind("/")
     return url_image[:(index + 1)] + "120-" + url_image[(index + 1):]
 
-def write_csv(warranty, price, image_item_resize, item_infos):
+def write_csv(item):
     with open("temp.csv", mode="a") as csv_file:
-        fieldnames = ["warranty", 
-                      "price", 
-                      "image_item_resize",
-                      "item_infos"]
+        fieldnames = ["title",
+                  "warranty", 
+                  "price", 
+                  "image_item_origin",
+                  "image_item_resize",
+                  "item_infos",
+                  "item_infos_technical"]
         writer = csv.DictWriter(csv_file, fieldnames)
 
-        writer.writeheader()
-        writer.writerow({"warranty": str(warranty),
-                         "price": str(price),
-                         "image_item_resize": str(image_item_resize),
-                         "item_infos": str(item_infos)})
+        if (item == None):
+            writer.writerow({"title": 'None',
+                            "warranty": 'None',
+                            "price": 'None',
+                            "image_item_origin": 'None',
+                            "image_item_resize": 'None',
+                            "item_infos": 'None',
+                            "item_infos_technical": 'None'})
+        else:
+            writer.writerow({"title": item.get_title(),
+                            "warranty": item.get_warranty(),
+                            "price": item.get_price(),
+                            "image_item_origin": item.get_item_image_origin(),
+                            "image_item_resize": str(item.get_item_image_resize()),
+                            "item_infos": str(item.get_item_infos()),
+                            "item_infos_technical": str(item.get_item_infos_technical())})
 
 def convert_utf8_to_plain(text):
     return bytes(text, 'utf-8').decode('utf-8')
@@ -56,22 +71,36 @@ def get_data_of_an_item(url_item):
     image_item_origin = []
     item_infos = []
     item_infos_technical = []
+    item = philong.Item()
 
+    error_not_found = html_page.find("div", attrs={"class": "agile-info"})
+    if (error_not_found != None and error_not_found.find('h2') != None):
+        write_csv(None)
+        return
 
+    ## Get title
+    title = html_page.find("div", attrs={"class": "entry-header"}).find('h1').text
+    #print("Tieu he: " + title)
+    item.set_title(title)
+
+    ## Get warranty
     temp = html_page.find("div", attrs={"class": "entry-header"}).find_all("span")
     if (len(temp) == 4):
         warranty = temp[2].text
         warranty = warranty[(warranty.rfind(":") + 1):].strip()
-    print("Bao hanh: " + warranty)
+        
+    #print("Bao hanh: " + str(warranty))
+    item.set_warranty(warranty)
 
-
+    ## Get price
     temp = html_page.find("div", attrs={"class": "entry-summary"})\
                     .find("span", attrs={"class": "p-price"})\
                     .find("span").text
     price = temp[:(len(temp) - 1)].replace('.', '')
-    print("Price: " + price)
+    #print("Price: " + price)
+    item.set_price(price)
 
-
+    ## Get image
     temp = html_page.find("div", attrs={"id": "sync2"})\
                     .find_all("div", attrs={"class": "item text-center"})
     if (len(temp) == 0):
@@ -85,50 +114,137 @@ def get_data_of_an_item(url_item):
             origin_image_url = base_url + div.a["href"]
             image_item_origin.append(origin_image_url)
             image_item_resize.append(get_resize_image_url(origin_image_url))
-    print(image_item_origin)
-    print(image_item_resize)
-
-    temp = html_page.find("div", attrs={"class": "pro-info"})\
-                    .find_all("p")
-    if (len(temp) != 0):
-        for p in temp:
-            item_infos.append(p.text)
-    print(item_infos)
-
-    temp = html_page.find("div", attrs={"id": "product_spec"})\
-                    .find("div", attrs={"class": "tbl-technical"})\
-                    .find("table")
-
-    trs = temp.find_all("tr")
-    for tr in trs:
-        item_info_technical = {}
-
-        tds = tr.find_all("td")
-        
-        # title of item_info_technical
-        title = convert_utf8_to_plain(tds[0].text)
-        item_info_technical['title'] = title#.replace('\n', '\\n')
-        
-        # value of item_info_technical
-        if (len(tds) > 1): 
-            value = convert_utf8_to_plain(tds[1].text)
-            item_info_technical['value'] = value#.replace('\n', '\\n')
-        
-        item_infos_technical.append(item_info_technical)
-
-    a.temp_response = item_infos_technical
-    print(json.dumps(item_infos_technical))
-
-
-    temp = html_page.find()
-
-    write_csv(warranty= warranty, price= price, image_item_resize= image_item_resize, item_infos= item_infos)
     
-get_data_of_an_item("https://philong.com.vn/laptop-acer-nitro5-an515-54-784p-core-i7-9750h-ram-8gb-hdd-1tb-man-hinh-15.6quot-fhd-1650-4g-win-10.html")
+    #print(json.dumps(image_item_origin))
+    item.set_item_image_origin(json.dumps(image_item_origin))
 
-# @app.route('/user')
-# def recommendation_items_for_user():
-#     return Response(json.dumps(a.temp_response),  mimetype='application/json')
+    #print(json.dumps(image_item_resize))
+    item.set_item_image_resize(json.dumps(image_item_resize))
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000) #run app in debug mode on port 5000
+    temp = html_page.find("div", attrs={"class": "pro-info"})
+
+    if (temp != None):
+        temp = temp.find_all("p")
+
+        if (len(temp) != 0):
+            for p in temp:
+                item_infos.append(p.text)
+        
+        #print(item_infos)
+        item.set_item_infos(item_infos)
+    else:
+        item.set_item_infos(str(None))
+
+    try:
+        temp = html_page.find("div", attrs={"id": "product_spec"})\
+                        .find("div", attrs={"class": "tbl-technical"})
+
+        if (temp != None):
+            temp = temp.find("table")
+
+        if (temp != None):           
+
+            trs = temp.find_all("tr")
+            for tr in trs:
+                item_info_technical = {}
+
+                tds = tr.find_all("td")
+                
+                if (len(tds) == 0):
+                    tds = tr.find_all("th")
+
+                # title of item_info_technical
+                title = convert_utf8_to_plain(tds[0].text)
+                item_info_technical['title'] = title#.replace('\n', '\\n')
+                
+                # value of item_info_technical
+                if (len(tds) > 1): 
+                    value = convert_utf8_to_plain(tds[1].text)
+                    item_info_technical['value'] = value#.replace('\n', '\\n')
+                
+                item_infos_technical.append(item_info_technical)
+
+            #print(json.dumps(item_infos_technical))
+            item.set_item_infos_technical(json.dumps(item_infos_technical))
+        else:
+            item.set_item_infos_technical(str(None))
+    except:
+        print("An exception technical infos")
+        item.set_item_infos_technical(str(None))
+
+    write_csv(item)
+    
+def get_all_url_items(urls_and_names):
+    urls = []
+    for url_and_name in urls_and_names:
+        url = url_and_name[0]
+        condition = True
+        page = 1
+        while(condition):
+            response = urllib.request.urlopen(url + "?page=" + str(page))
+            html_page = BeautifulSoup(response.read().decode('utf-8'), "html.parser")
+
+            product_item_in_list = html_page.find("div", attrs={"id": "product_item_in_list"})
+
+            if (product_item_in_list == None):
+                condition = False
+            else:
+                print("page " + str(page))
+                page = page + 1
+                product_items = product_item_in_list.find_all("h4", attrs={"class": "p-name"})
+                for product_item in product_items:
+                    urls.append(base_url + product_item.find('a')['href'])
+                    print("url " + base_url + product_item.find('a')['href'])
+
+    return urls
+
+def write_urls_to_file(urls_and_names):
+    urls = get_all_url_items(urls_and_names)
+
+    with open("urls.csv", mode="a") as csv_file:
+        fieldnames = ["ID", "url"]
+        writer = csv.DictWriter(csv_file, fieldnames)
+        writer.writeheader()
+        id = 0
+
+        for url in urls:
+            writer.writerow({"ID": id,
+                            "url": url})
+            
+            id = id + 1
+
+def get_all_urls_from_file(file_name):
+    urls = []
+    
+    with open(file_name) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+
+        line = 0
+        for row in csv_reader:
+            if(line != 0):
+                urls.append(row[1])
+            else:
+                line += 1
+
+    return urls
+
+urls = get_all_urls_from_file("urls.csv")
+
+# with open("temp.csv", mode="a") as csv_file:
+#     fieldnames = ["title",
+#                   "warranty", 
+#                   "price", 
+#                   "image_item_origin",
+#                   "image_item_resize",
+#                   "item_infos",
+#                   "item_infos_technical"]
+#     writer = csv.DictWriter(csv_file, fieldnames)
+#     writer.writeheader()
+
+index = 1
+for url in urls:
+    print(index)
+    get_data_of_an_item(url)
+    index = index + 1
+
+#get_data_of_an_item("https://philong.com.vn/laptop-acer-nitro5-an515-54-784p-core-i7-9750h-ram-8gb-hdd-1tb-man-hinh-15.6quot-fhd-1650-4g-win-10.html")
--- crawl/ngoc/philong.py
+++ crawl/ngoc/philong.py
@@ -1,4 +1,11 @@
-class PhiLong:
+class Item:
+
+  def set_title(self, title):
+    self.title = title;
+
+  def get_title(self):
+    return self.title
+
   def set_warranty(self, warranty):
     self.warranty = warranty
   
@@ -11,16 +18,28 @@ class PhiLong:
   def get_price(self):
     return self.price
 
-  def set_item_image(self, item_image):
-    self.item_image = item_image
+  def set_item_image_origin(self, item_image_origin):
+    self.item_image_origin = item_image_origin
 
-  def get_item_image(self):
-    return self.item_image
+  def get_item_image_origin(self):
+    return self.item_image_origin
+
+  def set_item_image_resize(self, item_image_resize):
+    self.item_image_resize = item_image_resize
+
+  def get_item_image_resize(self):
+    return self.item_image_resize
 
   def set_item_infos(self, item_infos):
     self.item_infos = item_infos
 
   def get_item_infos(self):
     return self.item_infos
+
+  def set_item_infos_technical(self, item_infos_technical):
+    self.item_infos_technical = item_infos_technical
+
+  def get_item_infos_technical(self):
+    return self.item_infos_technical
 
   
